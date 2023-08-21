458df295ba4775e341d8f642269bf229f988b7ea
manonBr <manon.bruimaud@gmail.com>
2023-08-21 16:39:28 +0200

    :refactor: Adding JsDoc to elements

--- client/src/features/elements/Headings.features.jsx
+++ client/src/features/elements/Headings.features.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import "./Headings.features.scss"
 
+/**
+ * 
+ * @param {Object} props 
+ * @param {string} props.level -- "Primary or secondary title"
+ * @param {string} props.children 
+ * @returns 
+ */
 const Heading = ({level, children}) => {
 
     return (
@@ -8,9 +15,16 @@ const Heading = ({level, children}) => {
     )
 }
 
-const Subheading = ({children}) => {
+/**
+ * 
+ * @param {Object} props 
+ * @param {string} props.level -- "Primary or secondary subttl"
+ * @param {string} props.children 
+ * @returns 
+ */
+const Subheading = ({level, children}) => {
     return (
-        <h2 className="subttl">{children}</h2>
+        <h2 className={`subttl subttl--${level}`}>{children}</h2>
     )
 }
 
