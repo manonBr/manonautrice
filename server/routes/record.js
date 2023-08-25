1975453979510e0a4f440141b21144101d79aa8d
manonBr <manon.bruimaud@gmail.com>
2023-08-25 15:42:18 +0200

    :feat: Adding axios configuration and /content route in server

---
+++ server/routes/record.js
@@ -0,0 +1,97 @@
+const express = require("express");
+ 
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../db/conn");
+ 
+// This helps convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the records.
+recordRoutes.route("/content").get(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb("manonautrice");
+    const result = await db_connect.collection("ma_textcontent").find({}).toArray();
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+
+recordRoutes.route("/record").get(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb("employees");
+    const result = await db_connect.collection("records").find({}).toArray();
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+ 
+// This section will help you get a single record by id
+recordRoutes.route("/record/:id").get(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb("employees");
+    let myquery = { _id: ObjectId(req.params.id) };
+    const result = await db_connect.collection("records").findOne(myquery);
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+ 
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb();
+    let myobj = {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    };
+    const result = await db_connect.collection("records").insertOne(myobj);
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+ 
+// This section will help you update a record by id.
+recordRoutes.route("/update/:id").post(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    let newvalues = {
+      $set: {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+      },
+    };
+    const result = await db_connect.collection("records").updateOne(myquery, newvalues);
+    console.log("1 document updated");
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+ 
+// This section will help you delete a record
+recordRoutes.route("/:id").delete(async function (req, res) {
+  try {
+    const db_connect = await dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    const result = await db_connect.collection("records").deleteOne(myquery);
+    console.log("1 document deleted");
+    res.json(result);
+  } catch (err) {
+    throw err;
+  }
+});
+ 
+module.exports = recordRoutes;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require("express")
-const app = express()
 const cors = require("cors")
+
+const app = express()
 require("dotenv").config({ path: "./config.env" })
 const port = process.env.PORT || 5000
 
+// Middlewares
 app.use(cors())
 app.use(express.json())
+
+// Routes
 app.use(require("./routes/record"))
 
 // Get MongoDB driver connection
 const dbo = require("./db/conn")
+
+// API
+app.get("/", (req, res) => res.status(200).send("Home page"))
  
 app.listen(port, () => {
   // Perform a database connection when server starts
