f2696f0fb96663d971cba3b823ee28ac111c533e
manonBr <manon.bruimaud@gmail.com>
2023-08-31 01:12:17 +0200

    :feat: Update menu animations

--- client/src/components/Header/Nav.jsx
+++ client/src/components/Header/Nav.jsx
@@ -1,50 +1,82 @@
-import * as React from "react"
+import React, { useRef, useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { useMobileSize } from "../../hooks/useScreenSize"
 import "./Nav.scss"
 
 const Nav = () => {
+    const hamburgerMenuIcon = useRef()
+    const menu = useRef()
+    const menuLinkWithChildren = useRef()
+    const subMenu = useRef()
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false)
+    const isMobile = useMobileSize()
 
-    const handleMenuHamburgerClick = (e) => {
-        const checked = e.target.checked
-        if(checked) {
-            document.querySelector('.menu__container').classList.remove('s-hidden')
+    useEffect(() => {
+        window.addEventListener('mousedown', closeSubMenu)
+        return () => {
+            window.removeEventListener('mousedown', closeSubMenu)
+        }
+    }, [isSubMenuOpen])
+    
+    const closeMenuHamburger = () => {
+        menu.current.classList.add('s-hidden')
+        hamburgerMenuIcon.current.checked = false
+        setIsMenuOpen(false)
+    }
+
+    const closeSubMenu = (e) => {
+        if(
+            subMenu.current && isSubMenuOpen
+            && subMenu.current.classList.contains("visible")
+            && !subMenu.current.contains(e.target) 
+            && e.target !== menuLinkWithChildren.current
+        ){
+            subMenu.current.classList.remove('visible')
+            setIsSubMenuOpen(false)
+        }
+    } 
+    
+    const handleClickMenuHamburger = () => {
+        if(isMenuOpen) {
+            closeMenuHamburger()
         } else {
-            document.querySelector('.menu__container').classList.add('s-hidden')
+            menu.current.classList.remove('s-hidden')
+            setIsMenuOpen(true)
         }
     }
-    const handleMenuItemWithChildrenClick = (e) => {
-        const itemLink = e.target
-        const item = e.target.parentElement
-        const submenu = itemLink.nextElementSibling
-        const itemClassList = e.target.parentElement.classList
-        console.log(item.nextElementSibling)
-        if (submenu.classList.contains("visible")) {
-            submenu.classList.remove('visible')
+    const handleClickMenuItemWithChildren = (e) => {
+        e.preventDefault()
+        if (subMenu.current.classList.contains("visible")) {
+            subMenu.current.classList.remove('visible')
+            setIsSubMenuOpen(false)
         } else {
-            submenu.classList.add('visible')
+            subMenu.current.classList.add('visible')
+            setIsSubMenuOpen(true)
         }
     }
 
     return (
         <div className="menu">
-            <input type="checkbox" id="menu__hamburger" onClick={handleMenuHamburgerClick} />
+            <input type="checkbox" id="menu__hamburger"  onClick={handleClickMenuHamburger} ref={hamburgerMenuIcon}/>
             <label htmlFor="menu__hamburger" aria-label="Accès menu mobile">
                 <span></span>
                 <span></span>
                 <span></span>
             </label>
-            <div className="menu__container s-hidden loadHome" data-appear="1500">
+            <div className="menu__container s-hidden loadHome" data-appear="1500" ref={menu}>
                 <ul>
-                    <li className="menu__item"><Link to="/">Qui suis-je ?</Link></li>
-                    <li className="menu__item menu__item--hasChildren" onClick={handleMenuItemWithChildrenClick}>
-                        <Link to="/ressources">Ressources</Link>
-                        <ul>
-                            <li className="menu__subitem"><Link to="/ressources/templates-notion">Templates Notion</Link></li>
-                            <li className="menu__subitem"><Link to="/ressources/fiches-personnages">Fiches personnages</Link></li>
-                            <li className="menu__subitem"><Link to="/ressources/site-internet">Site internet : bonnes pratiques</Link></li>
+                    <li className="menu__item"><Link to="/"  onClick={isMobile ? closeMenuHamburger : undefined}>Qui suis-je ?</Link></li>
+                    <li className="menu__item menu__item--hasChildren" onClick={handleClickMenuItemWithChildren}>
+                        <a href="#" ref={menuLinkWithChildren}>Ressources</a>
+                        <ul ref={subMenu}>
+                            <li className="menu__subitem"><Link to="/ressources/templates-notion" onClick={isMobile ? closeMenuHamburger : undefined}>Templates Notion</Link></li>
+                            <li className="menu__subitem"><Link to="/ressources/fiches-personnages" onClick={isMobile ? closeMenuHamburger : undefined}>Fiches personnages</Link></li>
+                            <li className="menu__subitem"><Link to="/ressources/site-internet" onClick={isMobile ? closeMenuHamburger : undefined}>Site internet : bonnes pratiques</Link></li>
                         </ul>
                     </li>
-                    <li className="menu__item"><Link to="/projets">Projets en cours</Link></li>
+                    <li className="menu__item"><Link to="/projets" onClick={isMobile ? closeMenuHamburger : undefined}>Projets en cours</Link></li>
                     <li className="menu__item"><Link to="https://www.instagram.com/manonautrice/" target="_blank">Instagram</Link></li>
                     <li className="menu__item"><Link to="https://www.tiktok.com/@manon.autrice" target="_blank">TikTok</Link></li>
                 </ul>
