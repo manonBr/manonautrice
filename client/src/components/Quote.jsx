31165879c8561f138f55d53a825f36cdc9ef999e
manonBr <manon.bruimaud@gmail.com>
2023-09-03 17:33:13 +0200

    :feat: Add author to quotes

--- client/src/components/Quote.jsx
+++ client/src/components/Quote.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import "./Quote.scss"
 
-const Quote = ({children}) => {
+const Quote = ({from,children}) => {
     return (
         <section className="quotation">
             <p><span className="quotation__mark quotation__mark--left">«</span>{children}<span className="quotation__mark quotation__mark--right">»</span></p>
+            {from ? <p className="quotation__author">{from}</p> : ""}
         </section>
     )
 }
